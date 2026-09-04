617da58abdc33a6e6c0f94a15929301e876c612d
Matheus Eduardo Alves Ramalho <matheus.alves.ramalho@escola.pr.gov.br>
2026-09-04 11:51:11 -0300

    aa

--- script.js
+++ script.js
@@ -11,26 +11,29 @@ const perguntas = [
             {
                 texto: "Gosto de planejar detalhadamente antes de começar a codificar.",
                 afirmacao: "<b>Organização</b> é uma característica forte sua, valorizando o planejamento detalhado."
-            },
+            }
             {
                 texto: "Prefiro começar a codificar logo e ajustar o plano conforme avanço.",
-                afirmacao: "Você se destaca pela <b>dinamismo</b>, gostando de aprender e ajustar durante o processo."
+                afirmacao: "Você se destaca pelo <b>dinamismo</b>, gostando de aprender e ajustar durante o processo."
             }
         ]
-    },
+    }
+
     {
         enunciado: "Como você lida com problemas ou bugs no seu código?",
         alternativas: [
             {
                 texto: "Gosto de resolver os problemas sozinho, pesquisando e testando diferentes soluções.",
                 afirmacao: "Independência define sua abordagem para resolver problemas, confiando nas suas <b>habilidades de pesquisa</b>."
-            },
+            }
+
             {
                 texto: "Prefiro pedir ajuda a colegas ou em fóruns de programação quando encontro dificuldades.",
                 afirmacao: "Colaboração é essencial para você, apreciando a <b>troca de conhecimento</b> com outros programadores."
             }
         ]
     },
+
     {
         enunciado: "Qual é a sua preferência em relação à estética versus funcionalidade em um projeto?",
         alternativas: [
@@ -44,6 +47,7 @@ const perguntas = [
             }
         ]
     },
+
     {
         enunciado: "Qual é a sua postura em relação ao aprendizado de novas tecnologias?",
         alternativas: [
@@ -56,7 +60,8 @@ const perguntas = [
                 afirmacao: "<b>Curiosidade</b> é uma marca sua, sempre em busca de novas ferramentas e técnicas."
             }
         ]
-    },
+    }
+
     {
         enunciado: "Como você prefere trabalhar em um projeto?",
         alternativas: [
@@ -71,42 +76,48 @@ const perguntas = [
         ]
     }
 ];
-
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
-function mostraPergunta(){
-    if(atual >= perguntas.length){
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
         mostraResultado();
         return;
     }
+
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
+function mostraAlternativas() {
+
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas)
+        botaoAlternativas.addEventListener(
+            "click",
+            () => respostaSelecionada(alternativa)
+        );
+
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
-function respostaSelecionada(opcaoSelecionada){
+function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
 
-function mostraResultado(){
-    caixaPerguntas.textContent = "Olha só o que podemos afirmar sobre você...";
+function mostraResultado() {
+    caixaPerguntas.textContent =
+        "Olha só o que podemos afirmar sobre você...";
     textoResultado.innerHTML = historiaFinal;
     caixaAlternativas.innerHTML = "";
 }
 
-mostraPergunta(); 
+mostraPergunta();
